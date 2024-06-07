f16709aa5d45d0411dec16b00f6a3d4b3793aaef
Ansh0505 <110764319+Ansh0505@users.noreply.github.com>
2024-06-07 12:18:11 +0530

    Add files via upload

---
+++ EventNumber.java
@@ -0,0 +1,12 @@
+public class EventNumber
+{
+	public static void main(String[] args)
+	{
+		int[]arr= {21,464,21,4656,23,67,2987,40};
+		//write code to display all event numbers from the array
+		for(int i=0; i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+}
