c04df431144d709189cbac2d68dd339c27396d38
Ansh0505 <110764319+Ansh0505@users.noreply.github.com>
2024-06-07 12:17:10 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,17 @@
+public class Demo
+{
+	public static void main(String[] args)
+	{
+		int a[]= new int[5];
+		for(int i=0;i<5;i++)
+		{
+			a[i]=i;
+		}
+		for (int i:a)
+		{
+			System.out.println(i);
+		}
+			
+	}
+
+}
