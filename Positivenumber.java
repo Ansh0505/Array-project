283b0a454cd19a0233bfc939d94e66517729cd71
Ansh0505 <110764319+Ansh0505@users.noreply.github.com>
2024-06-07 12:18:52 +0530

    Add files via upload

---
+++ Positivenumber.java
@@ -0,0 +1,16 @@
+public class Positivenumber
+{
+	public static void main(String[] args)
+	{
+		int[]arr= {19,-464,21,4656,-23,0,2,-987,40};
+		//Write a code to display all positive number from the array
+		for(int i=0; i<arr.length;i++)
+		{
+			if(arr[i]>0)
+			{
+			 System.out.println(arr[i]);
+			}
+		}
+	}
+
+}
